b4335e1fef64cc7cb0874de8847ff37e01c5a5d2
A R <iizekalfthi@gmail.com>
2021-06-15 19:37:32 +0400

    adding products photos

--- website/settings.py
+++ website/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coffeedb',
+        'USER': 'postgres',
+        'PASSWORD': 'otaku',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
